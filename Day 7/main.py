83a580d8ac52bd274a4e61f6ec20ceb42b500774
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2024-12-16 07:39:19 +0100

    Day7 Part2

--- Day 7/main.py
+++ Day 7/main.py
@@ -1,4 +1,21 @@
+def next_possibilities(numbers: list[int], base: int = 2) -> list[int]:
+    numbers[-1] += 1
+    for i in range(len(numbers)):
+        if numbers[-i-1] == base:
+            try:
+                numbers[-i-2] += 1
+                numbers[-i-1] = 0
+            except IndexError:
+                for j in range(len(numbers)):
+                    numbers[j] = 0
+                break
+
+    return numbers
+
+
 def can_made_this_euqation(equation: list[int], answer: int) -> bool:
+    if len(equation) == 1:
+        return equation[0] == answer
 
     max_pos = 2 ** (len(equation)-1)
     for i in range(max_pos):
@@ -16,6 +33,28 @@ def can_made_this_euqation(equation: list[int], answer: int) -> bool:
     return False
 
 
+def can_made_this_euqation2(equation: list[int], answer: int) -> bool:
+    max_pos = 3 ** (len(equation)-1)
+    pos = [0 for i in range(len(equation)-1)]
+    
+    for i in range(max_pos):
+        sum = equation[0]
+        for j in range(len(pos)):
+            if pos[j] == 0:
+                sum *= equation[j+1]
+            elif pos[j] == 1:
+                sum += equation[j+1]
+            else:
+                sum = int(str(sum)+str(equation[j+1]))
+        
+        if sum == answer:
+            return True
+
+        pos = next_possibilities(pos, base=3)
+
+    return False
+
+
 if __name__ == "__main__":
     equations: list[list[int]] = []
     answers: list[int] = []
@@ -28,8 +67,11 @@ if __name__ == "__main__":
             equations[-1] = list(map(int, equations[-1]))
 
     sum = 0
+    sum2 = 0
     for i in range(len(answers)):
         if can_made_this_euqation(equations[i], answers[i]):
             sum += answers[i]
-
+        elif can_made_this_euqation2(equations[i], answers[i]):
+            sum2 += answers[i]
     print(sum)
+    print(sum+sum2)
--- Day 7/test_main.py
+++ Day 7/test_main.py
@@ -1,7 +1,15 @@
 import unittest
-from main import can_made_this_euqation
+from main import can_made_this_euqation, can_made_this_euqation2, next_possibilities
 
 class TestCalibrationProcess(unittest.TestCase):
+    def test_helper_function_get_true(self):
+        self.assertEqual(next_possibilities([0, 0, 0], 2), [0, 0, 1])
+        self.assertEqual(next_possibilities([0, 0, 1], 2), [0, 1, 0])
+        self.assertEqual(next_possibilities([0, 1, 1], 2), [1, 0, 0])
+        self.assertEqual(next_possibilities([0, 0, 1], 3), [0, 0, 2])
+        self.assertEqual(next_possibilities([0, 2, 2], 3), [1, 0, 0])
+        self.assertEqual(next_possibilities([0, 2, 0], 3), [0, 2, 1])
+        self.assertEqual(next_possibilities([2, 2, 2], 3), [0, 0, 0])
 
     def test_first_sample(self):
         equation = [
@@ -24,6 +32,28 @@ class TestCalibrationProcess(unittest.TestCase):
         self.assertEqual(can_made_this_euqation(equation[6], 192), False)
         self.assertEqual(can_made_this_euqation(equation[7], 21_037), False)
         self.assertEqual(can_made_this_euqation(equation[8], 292), True)
+
+    def test_second_sample(self):
+        equation = [
+            [10, 19],
+            [81, 40, 27],
+            [17, 5],
+            [15, 6],
+            [6, 8, 6, 15],
+            [16, 10, 13],
+            [17, 8, 14],
+            [9, 7, 18, 13],
+            [11, 6, 16, 20]
+        ]
+        self.assertEqual(can_made_this_euqation2(equation[0], 190), True)
+        self.assertEqual(can_made_this_euqation2(equation[1], 3_267), True)
+        self.assertEqual(can_made_this_euqation2(equation[2], 83), False)
+        self.assertEqual(can_made_this_euqation2(equation[3], 156), True)
+        self.assertEqual(can_made_this_euqation2(equation[4], 7_290), True)
+        self.assertEqual(can_made_this_euqation2(equation[5], 161_011), False)
+        self.assertEqual(can_made_this_euqation2(equation[6], 192), True)
+        self.assertEqual(can_made_this_euqation2(equation[7], 21_037), False)
+        self.assertEqual(can_made_this_euqation2(equation[8], 292), True)
 
 if __name__ == '__main__':
     unittest.main()
